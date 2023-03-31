af030c1d701513d6b4aebcc425bbd2192f83410d
ptm99 <phamtranminh@gmail.com>
2023-03-31 09:49:38 +0700

    geoserver included

---
+++ python_app/fieldphotos_QGIS/exifimport.py
@@ -0,0 +1,24 @@
+def exifout(fname):
+    import os, pyheif, 
+
+
+
+
+
+# import pyheif
+# from PIL import Image
+
+# # Open the HEIF file using pyheif
+# heif_file = pyheif.read(&apos;image.heic&apos;)
+
+# # Extract metadata from the HEIF file
+# metadata = heif_file.metadata
+
+# # Get GPS information from metadata
+# gps_info = metadata[&apos;gps&apos;]
+
+# # Get latitude and longitude from GPS information
+# latitude = gps_info[&apos;latitude&apos;]
+# longitude = gps_info[&apos;longitude&apos;]
+
+# print(f&apos;Latitude: {latitude}, Longitude: {longitude}&apos;)
