0cdeda002ab2f8f163e4ad5d99541ac759bcd527
ptm99 <phamtranminh@gmail.com>
2023-04-03 19:14:49 +0700

    finish read exif to geojson

--- python_app/fieldphotos_QGIS/exifimport.py
+++ python_app/fieldphotos_QGIS/exifimport.py
@@ -33,21 +33,4 @@ t=tags['Image DateTime'].values
 b['year']=
 b['month']=
 b['beacon_deg']=-1
-b['focal_length35mm']=
-# import pyheif
-# from PIL import Image
-
-# # Open the HEIF file using pyheif
-# heif_file = pyheif.read(&apos;image.heic&apos;)
-
-# # Extract metadata from the HEIF file
-# metadata = heif_file.metadata
-
-# # Get GPS information from metadata
-# gps_info = metadata[&apos;gps&apos;]
-
-# # Get latitude and longitude from GPS information
-# latitude = gps_info[&apos;latitude&apos;]
-# longitude = gps_info[&apos;longitude&apos;]
-
-# print(f&apos;Latitude: {latitude}, Longitude: {longitude}&apos;)
+b['focal_length35mm']=0
